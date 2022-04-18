847330928fecf8d085e0fa11efde5748b1e93e2f
andrewapp <andrew.appuhamy@gmail.com>
2022-04-19 01:53:27 +1000

    on hover button

---
+++ README.md
@@ -0,0 +1,8 @@
+Built with UI Components from Ionic Framework Version 6.
+
+Tech-Stack:
+
+-React.ts
+-Postgres
+-Ktor
+-Exposed ORM
--- src/components/ReorderButton.tsx
+++ src/components/ReorderButton.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {IonButton, IonText} from '@ionic/react';
+import "./ReorderButton.css"
 
 export const ReorderButton: React.FC = () => (
 
-<IonButton color="success" shape="round" expand="block">
+<div className="Reorder ion-text-center">
+<IonButton color="success" shape="round">
     <IonText color="dark"> Reorder Playlist </IonText>
 </IonButton>
-
-
+</div>
 );
