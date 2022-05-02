be314f8e4dde67a52b95f0132be5a7a0713c7b91
andrew-app <andrew-app>
2022-05-02 10:35:38 +1000

    client id

--- Backend/Organoiser/src/main/kotlin/Main.kt
+++ Backend/Organoiser/src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import com.adamratzman.spotify.*
+import java.io.File
 
-suspend fun main() {
+fun main() {
     val codeVerifier = "thisisaveryrandomalphanumericcodeverifierandisgreaterthan43characters"
     val codeChallenge = getSpotifyPkceCodeChallenge(codeVerifier) // helper method
     val url: String = getSpotifyPkceAuthorizationUrl(
@@ -8,9 +9,12 @@ suspend fun main() {
         SpotifyScope.PLAYLIST_MODIFY_PRIVATE,
         SpotifyScope.USER_FOLLOW_READ,
         SpotifyScope.USER_LIBRARY_MODIFY,
-        clientId = "",
+        clientId = readFileDirectlyAsText("src/main/resources/clientID.txt"),
         redirectUri = "http://localhost:8888/callback",
         codeChallenge = codeChallenge
     )
     println(url)
 }
+
+fun readFileDirectlyAsText(fileName: String): String
+        = File(fileName).readText(Charsets.UTF_8)
